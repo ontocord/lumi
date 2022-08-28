7057c1f9681108134370813a75f6f131ffa8fdd0
ontocord <ontocord@gmail.com>
2022-08-28 13:54:52 -0700

    Update create_synthetic_text_image.py

--- create_synthetic_text_image.py
+++ create_synthetic_text_image.py
@@ -479,7 +479,7 @@ def create_synthetic_text_image_data(output_append_to_file, input_en_txt_gz_file
                         mult = 1.0
                         generated_sentence, aug2ent  = augment_ents(generated_sentence, do_person=False, do_loc=True, do_obj=True, other_person_list=other_person_list)
                       else:
-                        multi = 1.25
+                        mult = 1.25
                       generated_sentence = image_type +generated_sentence
                       #generate an image 
                       tokens, img = minidalle.generate(generated_sentence, image_output=True, token_output=True)
@@ -489,7 +489,7 @@ def create_synthetic_text_image_data(output_append_to_file, input_en_txt_gz_file
                       print ("generated augmented", generated_sentence)
                       generated_sentence = orig_generated_sentence
                       matched_output2 = get_decomposed_sent_to_img(generated_sentence, img)
-                      if matched_output2 and matched_output2['score'] >= multi*score_cutoff and len([a for a in matched_output2['element2text'].values() if a[1] >= score_cutoff]) >= (len(matched_output2['element2text'])*.5):
+                      if matched_output2 and matched_output2['score'] >= mult*score_cutoff and len([a for a in matched_output2['element2text'].values() if a[1] >= score_cutoff]) >= (len(matched_output2['element2text'])*.5):
                         matched_output['tokens2'] = tokens.tostring()
                         matched_output['thumbnail2'] = np.array(img).tostring()
                         matched_output['element2text2'] = matched_output2['element2text']
