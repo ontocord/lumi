6d78fd6302f41bd0e49550e4fd25dc9ee2bb7df8
ontocord <ontocord@gmail.com>
2022-08-31 13:30:13 -0700

    Update create_synthetic_text_image.py

--- create_synthetic_text_image.py
+++ create_synthetic_text_image.py
@@ -599,11 +599,12 @@ def create_synthetic_text_image_data(output_append_to_file, input_en_txt_gz_file
                         mult = 1.25
                         prob_add_qa_image_type = 1.0
                         qa_list = []
+                      prefix = ""
                       if mood_type and not image_type:
                         prefix =  mood_type + " picture of:" 
                       elif not mood_type and image_type:
                         prefix = image_type +" of:"
-                      else:
+                      elif mood_type and image_type:
                         prefix = mood_type + " " + image_type +" of:"
                       print (prefix)
                       if prefix:
