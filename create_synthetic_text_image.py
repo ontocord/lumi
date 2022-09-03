3f598798f86f9243dc1133a3a02f6ac014dce7a5
ontocord <ontocord@gmail.com>
2022-09-03 11:26:09 -0700

    Update create_synthetic_text_image.py

--- create_synthetic_text_image.py
+++ create_synthetic_text_image.py
@@ -257,7 +257,7 @@ def get_element_to_img(matched_sentence, img, ignore_from_box=[], other_element_
     text4 = text5 
   if text4:
     if get_box_images:
-      num_boxes = max(min_num_boxes, int(len(text4)/2)
+      num_boxes = max(min_num_boxes, int(len(text4)/2))
       normalized_boxes = decode_image(asarray(img), vlt5.frcnn,  vlt5.image_preprocessor, max_detections=num_boxes)["normalized_boxes"][0]
       
       clip_output = clip_image_to_multitext_score(clip_model, clip_processor, img, text4, decompose_image=True, normalized_boxes=normalized_boxes, ignore_from_box=ignore_from_box)  
